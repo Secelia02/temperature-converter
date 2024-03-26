faf8d68561f969f2c6068f70483e8dc76d3da58f
Secelia McNair <secelia@yahoo.com>
2024-03-26 16:47:05 -0500

    Added html and js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
 //Write your pseduo code first! 
+//Create an input for the temperature input
+//Get the formula for the conversion
+//Enter a temperature in Celsius
+//When you click on the button, the temperature will ouput in Fahrenheit using the conversion formula
+
+let submit = document.getElementById("submit").addEventListener("click", function() {
+  let temperature = document.getElementById("temperature").value
+  let outputTemp = document.getElementById("outputTemp")
+
+  outputTemp.innerHTML = (9/5 * temperature) + 32
+  
+})
